d7905ff975c4c3bd9b3c9d0fe7d4b533d1489114
adminks666 <xwcker521zy@gmail.com>
2022-08-30 10:40:17 +0800

    Update icpcode.js

--- js/icpcode.js
+++ js/icpcode.js
@@ -11,7 +11,3 @@
     Object.keys(currentObj).map(function (key) {
         $('.icpcode').html(currentObj.key)
     })
-
-注意：
-    // Object.keys(currentObj) 可以得到当前域名下的id1  id2  id3
-    $(key)最好使用#拼接一下，获取DOM，它与上面的id1  id2 相对应，循环设置每个id应该对应的文本内容，避免像第一种方法对每个div设置内容。
